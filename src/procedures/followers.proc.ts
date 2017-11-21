b392194ace087527c8fe2980a44534803e9ae61b
alabama56 <jakedownard@yahoo.com>
2017-11-21 16:52:34 -0600

    git is a nightmare

--- src/procedures/followers.proc.ts
+++ src/procedures/followers.proc.ts
@@ -1,5 +1,17 @@
-import { rows, row, empty, crud } from "./base.proc";
+import { rows, row, empty, crud, rowsets } from "./base.proc";
+import { SQL_GET } from "./base.proc";
+import { pluralize } from "../middleware/utils.mw";
 
 const MODEL_NAME = 'Follower';
 
-export const { create, destroy, all } = crud(MODEL_NAME);
+const additional_getFollowers = (MODEL_NAME: string) => {
+    return (...args: Array<any>) => {
+        return rows(`${SQL_GET}${pluralize(MODEL_NAME)}`, args)
+    };
+};
+
+const additionalProcedures = {
+    getFollowers: additional_getFollowers(MODEL_NAME),
+};
+
+export const { create, destroy, getFollowers } = crud(MODEL_NAME, additionalProcedures);
